db06b23887e3f44c8b0251ef98f0d71e4b283bb6
dinoproject <67891619+dinoproject@users.noreply.github.com>
2021-03-05 20:32:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="stylesheet.css">
+	<link rel="stylesheet" href="fontawesome/css/all.css">
+	<title>Tugas 2 DPW</title>
+</head>
+<body>
+	<header>
+		<p>daynoo</p>
+	</header>
+	<div class="content cf">
+		<div class="sidebar">
+			<p><a href="index.php">MyProject</a></p>
+			<p><a href="about.html">About me</a></p>
+			<p><a href="askme.html">Ask me!</a></p>
+		</div>
+		<div class="main">
+			<div class="table">
+				<h2>My Project</h2>
+				<table>
+					<thead>	
+						<th width="40px">NO.</th>
+						<th width="400px">PROJECT</th>
+						<th width="250px">AS</th>
+						<th>DESCRIPTION</th>
+					</thead>
+					<tbody>	
+						<tr>
+							<td>1.</td>
+							<td>AoT STORE</td>
+							<td>Programmer</td>
+							<td>AoT STORE adalah sebuah aplikasi penjualan perangkat komputer berbasis GUI menggunakan bahasa pemrogramman JAVA.</td>
+						</tr>
+						<tr>
+							<td>2.</td>
+							<td>Website YARSI (Tugas)</td>
+							<td>UI/UX Design</td>
+							<td>Sebuah website yang digunakan oleh mahasiswa YARSI untuk mengakses pembelajaran atau materi yang diberikan oleh dosen.
+							</td>
+						</tr>
+						<tr>
+							<td>3.</td>
+							<td>Form Login</td>
+							<td>Programmer</td>
+							<td>Sebuah project akhir dari mata kuliah TBO dimana harus membuat sebuah Form Login berbasis GUI dan Database menggunakan JAVA dan MySQL</td>
+						</tr>
+						<tr>
+							<td>4.</td>
+							<td>Landing Page SolMusic</td>
+							<td>Front End</td>
+							<td>Sebuah desain landing page aplikasi music dengan kemudahan akses berbagai lagu dengan mudah beserta lirik.</td>
+						</tr>
+					</tbody>
+					<tfoot>	</tfoot>
+				</table>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="icon-f">
+			<p>Instagram</p>
+			<a href="https://www.instagram.com/daynosaputraa/" target="_blank" ><img src="instagram.png" ></a>
+			<p>Github</p>
+			<a href="https://github.com/dinoproject" target="_blank"><img src="github.png"></a>
+			<p>Facebook</p>
+			<a href="https://www.facebook.com/onidkanahalokes/" target="_blank"><img src="facebook.png"></a>
+		</div>
+	</footer>
+</body>
+</html>
